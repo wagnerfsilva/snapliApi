7622378c6554186108f68197f946c4b7081dc18e
Wagner <wagnerdesignweb@gmail.com>
2026-05-22 13:40:24 -0300

    segurança: remove rota test-email sem autenticação

--- src/routes/download.routes.js
+++ src/routes/download.routes.js
@@ -26,7 +26,4 @@ router.post('/generate/:orderId', authenticate, downloadController.generateDownl
 // Resend download email
 router.post('/resend/:orderId', authenticate, downloadController.resendDownloadEmail);
 
-// TEST: Endpoint temporário para validar email em produção (remover depois)
-router.get('/test-email/:orderId', downloadController.resendDownloadEmail);
-
 module.exports = router;
